12a310e09c2231eb6809e637f8959f2d341eacca
Yahyagok <ygok17@hotmail.com>
2020-05-28 14:44:41 -0400

    start temp

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@
 // 
 
 document.addEventListener('DOMContentLoaded', (event) => {
+
 const content = document.getElementsByClassName('content')[0]
 const innerDoor = document.getElementsByClassName('inner-door')[0]
 
@@ -15,6 +16,7 @@ const doorPhone = document.getElementsByClassName('container')[2]
 const doorBtn = doorPhone.getElementsByTagName('button')[0]
 
 const washing = document.getElementsByClassName('washing')[0]
+const washingBtn = document.getElementsByClassName('washingBtn')[0]
 const body = document.getElementsByTagName('body')[0]
 
 allUtilities()
@@ -26,17 +28,26 @@ allUtilities()
       }
            function getUtilities(utilities){
               utilities.forEach(utility => {
-                console.log(utility)  
               const div = document.createElement('div')
               div.className = "each-utility"
               doorBtn.dataset.id = utility.id 
               const li = document.createElement('li')
+              const washingLi = document.createElement('li')
+
               if( utility.door === false){
                innerDoor.src = "photo/newdoor.jpg"
               }else{
                innerDoor.src = "photo/bluedoor1.jpg" 
               }
+              if (utility.washing === true){
+               washing.src = "photo/new machine1.jpg"
+              }else {
+               washing.src= "photo/washing machine.gif"
+              }
               li.innerHTML = utility.door
+              washingLi.innerHTML = utility.washing
+             washingLi.id = 'washing'
+              div.append(washingLi)
               div.append(li)
               body.append(div)
                    })
@@ -45,7 +56,10 @@ allUtilities()
 content.addEventListener('click', function(event){
 
             const utility = document.getElementsByClassName('each-utility')[0]
-        
+
+            const washingInfo = document.getElementById('washing')
+            
+            let washingStatus =  washingInfo.innerHTML
           
             const id = 3 
           //   const id = doorBtn.dataset.id
@@ -61,9 +75,17 @@ content.addEventListener('click', function(event){
                   doorBtn.textContent = "Close Door"    
                   status = "false"
             }
+            if (washingBtn.textContent === "stop washing"){
+                 washing.src = "photo/new machine1.jpg"
+                 washingBtn.textContent = "run washing"
+                  washingStatus = "true"
+                 
+            }else if (event.target.className === 'washingBtn') {
+                    washing.src = 'photo/washing machine.gif'
+                    washingBtn.textContent = "stop washing"
+                      washingStasus = "false"
+               }
 
-          
-        
               fetch(`http://localhost:3000/utilities/${id}`,{
 
                    method: "PATCH",
@@ -71,49 +93,18 @@ content.addEventListener('click', function(event){
                               'Content-Type': 'application/json',
                               'Accept': 'application/json'       
                    },
-                   body: JSON.stringify({ door: status }) 
+                   body: JSON.stringify({ door: status, washing: washingStatus  }) 
              }) 
-             .then(r => r.json())
-             .then(data => {
-                   if (data.door === "true" ){
-                        innerDoor.src = "photo/newdoor.jpg"
-                        
-                      
-                   }else if(data.door === "false"){
-                        innerDoor.src = "photo/bluedoor1.jpg"  
-                   }
-             })
+
+             if (event.target.className === 'tempBtn'){
+                  
+             }
+             
+            
+       
+         
              
 
-            //  .then(r => r.json())
-            //  .then()
-
-          
-
-           
-            //  .then(r => r.json())
-            //  .then(utility => changeDoor(utility))
-
-      //     fetch(`http://localhost:3000/utilities/${id}`,{
-      //       method: "PATCH", 
-      //       body:JSON.stringify({ door:   })
-      // })
-
-      
-
-//    }
-
-    washingMachine()
-
-    function washingMachine(){
-        if (event.target.className == 'washingBtn' &&  event.target.innerHTML === 'Washing off' ){
-              washing.src= "photo/washing machine.gif"
-              event.target.innerHTML = "Washing on"
-        }else if (event.target.className == 'washingBtn' && event.target.innerHTML === 'Washing on') { 
-                washing.src = "photo/new machine1.jpg"
-                event.target.innerHTML = "Washing off"
-            }
-        }
 
    })
 
@@ -123,6 +114,24 @@ content.addEventListener('click', function(event){
      
 
    
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+});
 
 
 
@@ -144,24 +153,3 @@ content.addEventListener('click', function(event){
 
 
 
-
-    
-
-
-
-
-
-
-
-    
-});
-
-
-
-// if (event.target.innerHTML === 'open door') {
-           
-//     innerDoor.src = "photo/bluedoor1.jpg"
-//     event.target.innerHTML = "door close"
-//     console.log(event.target.innerHTML)
-    
-//    }
